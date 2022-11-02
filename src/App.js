c7cdf03eee38aefa2161d5a5157188498102b802
truong0235x <maxphe0235.com>
2022-11-02 11:18:18 +0700

    chenge func to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
